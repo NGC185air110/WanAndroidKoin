ce1dff87aee53ecf8aa1f7e58671ad987ff06d9b
dinglicheng <1297740798@qq.com>
2021-05-11 13:46:40 +0800

    BaseActivity

--- app/src/main/java/com/example/wanandroidkoin/MainActivity.kt
+++ app/src/main/java/com/example/wanandroidkoin/MainActivity.kt
@@ -1,32 +1,21 @@
 package com.example.wanandroidkoin
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.model_base.base.BaseActivity
 import com.example.model_base.model.ResultResponse
 import com.example.model_base.model.login.UserLoginBase
 import com.example.model_base.utils.RouteUtils
 import com.example.wanandroidkoin.databinding.ActivityMainBinding
 
 @Route(path = RouteUtils.Activity_Home)
-class MainActivity : AppCompatActivity(), MainContract.View {
-    private lateinit var binding: ActivityMainBinding
-    private var mainPresenter: MainPresenter? =MainPresenter(this)
+class MainActivity : BaseActivity<ActivityMainBinding>(), MainContract.View {
+    private var mainPresenter: MainPresenter? = MainPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LayoutInflater.from(this).run {
-            ActivityMainBinding.inflate(this)
-        }
         setContentView(binding.root)
-        binding.btnLogin.setOnClickListener {
-            mainPresenter?.login("1297740798@qq.com", "zhn820849zhn")
-        }
-        binding.btnFavorites.setOnClickListener {
-            mainPresenter?.getFavorites()
-        }
-        var a: Long = 197_812_309_120_931
     }
 
     override fun loginSuccess(userLoginBase: ResultResponse<UserLoginBase>) {
@@ -37,11 +26,28 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     }
 
-    override fun getFavoritesSuccess(favorites: ResultResponse<Any>) {
-        TODO("Not yet implemented")
+    override fun getFavoritesSuccess(favorites: ResultResponse<Any>) {}
+
+    override fun getFavoritesError() {}
+
+    override fun layoutId(): Int {
+        return R.layout.activity_main
     }
 
-    override fun getFavoritesError() {
-        TODO("Not yet implemented")
+    override fun initData() {}
+
+    override fun initListener() {
+        setOnClick(binding.btnLogin, binding.btnFavorites)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_login -> {
+                mainPresenter?.login("1297740798@qq.com", "zhn820849zhn")
+            }
+            R.id.btn_favorites -> {
+                mainPresenter?.getFavorites()
+            }
+        }
     }
 }
---
+++ model_base/src/main/java/com/example/model_base/base/BaseActivity.kt
@@ -0,0 +1,89 @@
+package com.example.model_base.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity<BindingClass> : AppCompatActivity(), View.OnClickListener {
+
+    private var innerBinding: BindingClass? = null
+
+    val binding: BindingClass by lazy { innerBinding!! }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val acName = javaClass.simpleName
+        val name = acName.substring(0, acName.indexOf("Activity"))
+        val bindingClass = classLoader.loadClass("com.example.wanandroidkoin.databinding.Activity${name}Binding")
+        innerBinding = bindingClass.getMethod("inflate", LayoutInflater::class.java).invoke(null, layoutInflater) as BindingClass
+
+        setContentView(layoutId())
+        initConfigure()
+        initView()
+        initListener()
+        initData()
+    }
+
+
+
+
+    abstract fun layoutId(): Int
+
+    /**
+     * 用于实现懒加载，只在当前页面可见时加载
+     *   * @param isVisible true  不可见 -> 可见
+     * false 可见  -> 不可见
+     */
+    protected open fun onActivityVisibleChange(isVisible: Boolean) {
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onActivityVisibleChange(true)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        onActivityVisibleChange(false)
+    }
+
+    /**
+     * 设置点击监听的方法
+     * @param view
+     */
+    fun setOnClick(vararg view: View) {
+        for (v in view) {
+            v.setOnClickListener(this)
+        }
+    }
+
+
+    /**
+     * 先于onCreate执行
+     */
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+    }
+
+    /**
+     * 初始化配置
+     */
+    private fun initConfigure() {}
+
+    /**
+     * 初始化数据
+     */
+    abstract fun initData()
+
+    /**
+     *初始化 View
+     */
+    open fun initView() {}
+
+    /**
+     * 初始化监听
+     */
+    abstract fun initListener()
+}
